0d17d1913510ee6131cf350beab0a9d51f957b4a
Cristian Paiva <cristianleopaiva@gmail.com>
2023-07-31 21:05:27 -0300

    Dando estilos los ItemToDos

--- ToDo-Project-with-React/src/components/CreateTodoButton/CreateTodoButton.js
+++ ToDo-Project-with-React/src/components/CreateTodoButton/CreateTodoButton.js
@@ -4,7 +4,7 @@ import plus from "../../images/icons/ico-plus.svg"
 
 function CreateTodoButton() {
   return(
-    <button>
+    <button className="Btn-create-task">
       <img src={ plus } alt={ "Create task" } />
       <span>Create task</span>
     </button>
--- ToDo-Project-with-React/src/components/TodoItem/TodoItem.js
+++ ToDo-Project-with-React/src/components/TodoItem/TodoItem.js
@@ -1,13 +1,18 @@
 import './TodoItem.css';
 
 import check from '../../images/icons/check.svg';
+import trash from '../../images/icons/ico-trash.svg';
+import edit from '../../images/icons/ico-edit.svg';
 
 function TodoItem( props ) {
   return(
     <li className="TodoItem TodoItem--complete">
-      <span className="Icon Icon-check Icon-check--active"><img src={ check } alt={ check } /></span>
+      <button className="Icon Icon-check Icon-check--active"><img src={ check } alt={ check } /></button>
       <p className="TodoItem-p TodoItem-p--complete">{ props.text }</p>
-      <span className="Icon Icon-delete">X</span>
+      <div className='Container-edit-delete'>
+        <button className="Icon Icon-edit"><img src={ edit } alt={ edit } /></button>
+        <button className="Icon Icon-delete"><img src={ trash } alt={ trash } /></button>
+      </div>
     </li>
   );
 }
